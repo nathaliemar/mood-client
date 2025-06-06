accfdad73bc05adb78ddee6b5fa7b81de28c19b5
nathaliemar <n.marten.95@gmail.com>
2025-06-06 18:40:54 +0200

    feat: implement authentication context and signup form with API integration

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import SignUpForm from "../components/SignUpForm";
+import { useAuthContext } from "../context/auth.context";
 
 function SignUpPage() {
-  return <div>SignUpPage</div>;
+  const { signup } = useAuthContext();
+  const [errorMsg, setErrorMsg] = useState();
+
+  const handleSignup = async (formData) => {
+    try {
+      await signup(formData);
+    } catch (error) {
+      setErrorMsg(error);
+    }
+  };
+
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <SignUpForm onSubmit={handleSignup} errorMsg={errorMsg} />
+      </div>
+    </div>
+  );
 }
 
 export { SignUpPage };
