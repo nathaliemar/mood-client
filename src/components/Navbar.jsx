257bb957e04e9aab761dc5c27b6926e91d7946b1
nathaliemar <n.marten.95@gmail.com>
2025-06-09 11:36:56 +0200

    fix: wrap MoodApp title in Link for navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,9 @@ const Navbar = () => {
   return (
     <nav className="sticky top-0 z-50 bg-white shadow-md">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between">
-        <div className="text-xl font-semibold text-gray-800">MoodApp</div>
+        <Link to={"/"}>
+          <div className="text-xl font-semibold text-gray-800">MoodApp</div>
+        </Link>
         <div className="space-x-4 hidden md:flex flex items-center">
           <Link to="/" className="text-gray-600 hover:text-gray-900">
             Dashboard
