446b287e9b56d36c334f3d3a0e4c206ce41728ec
nathaliemar <n.marten.95@gmail.com>
2025-06-09 12:39:20 +0200

    feat: add MoodEntryForm component with mood selection and comment submission; integrate confetti effect on submission

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { MoodEntryForm } from "../components/MoodEntryForm";
+import { api } from "../services/api";
+import Confetti from "react-confetti";
 
 function DashboardPage() {
-  return <div>Dashboard</div>;
+  const [errorMsg, setErrorMsg] = useState();
+  const [showConfetti, setShowConfetti] = useState(false);
+
+  const handleMoodEntry = async ({ mood, comment }) => {
+    const now = new Date();
+    now.setUTCHours(0, 0, 0, 0);
+    const reqBody = { score: mood, note: comment, date: now.toISOString() };
+    try {
+      await api.post("/api/moodentries", reqBody);
+      setShowConfetti(true); // Show confetti
+      setTimeout(() => setShowConfetti(false), 5000); // Hide after 5s
+    } catch (error) {
+      setErrorMsg(error);
+    }
+  };
+
+  //TODO: If error 409: you already submitted!
+
+  return (
+    <div>
+      {showConfetti && <Confetti />}
+      <h1>Hello there!</h1>
+      <div>
+        <MoodEntryForm onSubmit={handleMoodEntry} />
+      </div>
+    </div>
+  );
 }
 
 export { DashboardPage };
