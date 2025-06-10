da50643ed8b996cc28cc300c1a279a50a5fa8e6e
nathaliemar <n.marten.95@gmail.com>
2025-06-10 12:12:18 +0200

    feat: implement TeamListItem component with user fetching and editing functionality; update TeamListPage to display teams

--- src/pages/TeamListPage.jsx
+++ src/pages/TeamListPage.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { TeamListItem } from "../components/TeamListItem";
+import { Link } from "react-router-dom";
 
 function TeamListPage() {
-  return <div>TeamListPage</div>;
+  const [teams, setTeams] = useState([]);
+  const fetchTeams = async () => {
+    try {
+      const teams = await api.get("/api/teams");
+      setTeams(teams.data);
+    } catch (error) {
+      console.log(error);
+      setTeams([]);
+    }
+  };
+  useEffect(() => {
+    fetchTeams();
+  }, []);
+
+  return (
+    <>
+      <Link>
+        <button className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700 transition m-4">
+          Add new Team
+        </button>
+      </Link>
+      <div>
+        {teams.map((team) => (
+          <TeamListItem key={team._id} team={team} refreshTeams={fetchTeams} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export { TeamListPage };
--- src/pages/UserListPage.jsx
+++ src/pages/UserListPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../services/api";
 import { UserListItem } from "../components/UserListItem";
 import { Link } from "react-router-dom";
