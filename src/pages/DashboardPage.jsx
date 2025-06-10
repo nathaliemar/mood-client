8039f2a7c84418ba932604c7ea44e4a5c3b329d7
nathaliemar <n.marten.95@gmail.com>
2025-06-10 14:31:12 +0200

    feat: add navigation to DashboardPage after mood entry submission; fetch today's mood entry on user change

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,20 +1,53 @@
-import React, { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { MoodEntryForm } from "../components/MoodEntryForm";
 import { api } from "../services/api";
 import Confetti from "react-confetti";
+import { AuthContext } from "../context/auth.context";
 
 function DashboardPage() {
   const [errorMsg, setErrorMsg] = useState();
   const [showConfetti, setShowConfetti] = useState(false);
+  const [todayEntry, setTodayEntry] = useState();
+  const { user } = useContext(AuthContext);
+
+  //HELPER
+  const getTodayDateAtMidnight = () => {
+    const formattedDate = new Date();
+    formattedDate.setUTCHours(0, 0, 0, 0);
+    return formattedDate.toISOString();
+  };
+
+  //First thing: Get today's entry if there is one
+  const fetchTodayEntry = async () => {
+    try {
+      const res = await api.get(
+        `/api/moodentries/user/${
+          user._id
+        }/today?date=${getTodayDateAtMidnight()}`
+      );
+      setTodayEntry(res.data);
+    } catch (error) {
+      if (error.response?.status === 404) setTodayEntry(null); //expected
+      else setErrorMsg("Error fetching today's entry");
+    }
+  };
+  //call in useeffect
+  useEffect(() => {
+    if (!user?._id) return;
+    fetchTodayEntry();
+  }, [user]);
 
   const handleMoodEntry = async ({ mood, comment }) => {
-    const now = new Date();
-    now.setUTCHours(0, 0, 0, 0);
-    const reqBody = { score: mood, note: comment, date: now.toISOString() };
+    const reqBody = {
+      score: mood,
+      note: comment,
+      date: getTodayDateAtMidnight(),
+    };
     try {
-      await api.post("/api/moodentries", reqBody);
+      const res = await api.post("/api/moodentries", reqBody);
       setShowConfetti(true); // Show confetti
       setTimeout(() => setShowConfetti(false), 5000); // Hide after 5s
+      setTodayEntry(res.data);
     } catch (error) {
       setErrorMsg(error);
     }
@@ -26,9 +59,21 @@ function DashboardPage() {
     <div>
       {showConfetti && <Confetti />}
       <h1>Hello there!</h1>
-      <div>
-        <MoodEntryForm onSubmit={handleMoodEntry} />
-      </div>
+      {todayEntry ? (
+        <div>
+          <p>
+            You already submitted for today!
+            <br />
+            Score: {todayEntry.score}
+            <br />
+            Note: {todayEntry.note}
+          </p>
+        </div>
+      ) : (
+        <div>
+          <MoodEntryForm onSubmit={handleMoodEntry} />
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { LoginForm } from "../components/LoginForm";
 import { useAuthContext } from "../context/auth.context";
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
   const { login, user } = useAuthContext();
   const [errorMsg, setErrorMsg] = useState();
+  const navigate = useNavigate();
 
   const handleLogin = async (formData) => {
     try {
       await login(formData);
+      navigate("/");
     } catch (error) {
       setErrorMsg(error);
     }
