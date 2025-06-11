36a0c2c8ec2e178b737e4a38da8f15ddb9ec28b2
nathaliemar <n.marten.95@gmail.com>
2025-06-11 21:10:30 +0200

    feat: add capitalize utility function and update role display in UserCard and UserListItem components

--- src/components/UserListItem.jsx
+++ src/components/UserListItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { capitalize } from "../utils/capitalize";
 
 function UserListItem({ user }) {
   return (
@@ -40,7 +41,9 @@ function UserListItem({ user }) {
         {user.teamLead ? "Yes" : "No"}
       </div>
       {/* Role */}
-      <div className="flex-1 min-w-0 text-gray-600 truncate">{user.role}</div>
+      <div className="flex-1 min-w-0 text-gray-600 truncate">
+        {capitalize(user.role)}
+      </div>
     </div>
   );
 }
