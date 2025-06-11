3ae2986f42fbc593e1bbfbef9d5731e7546ca974
nathaliemar <n.marten.95@gmail.com>
2025-06-11 10:26:15 +0200

    feat: add MoodEntryCard component to display today's mood entry details

---
+++ src/components/MoodEntryCard.jsx
@@ -0,0 +1,30 @@
+function MoodEntryCard({ moodEntry }) {
+  const {
+    createdBy: { imageUrl, firstName, lastName },
+    score,
+    note,
+  } = moodEntry;
+  return (
+    <div className="flex items-start gap-6 border border-gray-200 rounded-lg p-6 max-w-lg bg-white shadow">
+      <img
+        src={imageUrl}
+        alt={`${firstName} ${lastName}`}
+        className="w-20 h-20 rounded-full object-cover bg-gray-100"
+      />
+      <div className="w-full">
+        <div className="grid grid-cols-2 gap-y-3">
+          <div className="font-semibold text-left">First Name:</div>
+          <div className="text-left">{firstName}</div>
+          <div className="font-semibold text-left">Last Name:</div>
+          <div className="text-left">{lastName}</div>
+          <div className="font-semibold text-left">Score</div>
+          <div className="text-left">{score}</div>
+          <div className="font-semibold text-left">Note</div>
+          <div className="text-left">{note}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { MoodEntryCard };
