cabbb248ee3039c33c22f36e96ad1b8306625ff3
nathaliemar <n.marten.95@gmail.com>
2025-06-12 12:17:40 +0200

    fix: update page titles; enhance team mood entries display

--- src/pages/AnalyticsTeamPage.jsx
+++ src/pages/AnalyticsTeamPage.jsx
@@ -24,18 +24,23 @@ function AnalyticsTeamPage() {
   //Call in useeffect
   useEffect(() => {
     fetchTodaysEntries();
-    console.log(user);
   }, [user]);
 
   //todo: add page for users w/o team
   // todo: adjust context for admins
   return (
     <>
-      <h3>Today's mood Entries for your team: </h3>
-      <div>
-        {todayEntries.map((entry) => (
-          <MoodEntryCard moodEntry={entry} key={entry._id} />
-        ))}
+      <h3 className="text-3xl font-bold text-center my-6">
+        {`Today's mood Entries for ${user?.team?.teamName || "your"} team:`}
+      </h3>
+      <div className="flex justify-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {todayEntries.map((entry) => (
+            <div key={entry._id} className="m-4">
+              <MoodEntryCard moodEntry={entry} />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
